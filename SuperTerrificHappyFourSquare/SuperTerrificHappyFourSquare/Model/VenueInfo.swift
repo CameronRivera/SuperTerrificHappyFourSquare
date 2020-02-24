f49a1556d2773444ee3a03ea2182ea6de57e38ad
Chelsi Christmas <c.christmas95@gmail.com>
2020-02-24 12:42:44 -0500

    APIClient, UnitTest, Model

--- SuperTerrificHappyFourSquare/SuperTerrificHappyFourSquare/Model/VenueInfo.swift
+++ SuperTerrificHappyFourSquare/SuperTerrificHappyFourSquare/Model/VenueInfo.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+
+struct VenueData: Codable {
+    let response: VenueInfo
+}
+
+struct VenueInfo: Codable {
+    let venues: [Venue]
+}
+
+struct Venue: Codable {
+    let id: String
+    let name: String
+    let location: Location
+    let categories: [Category]
+//    let referralID: ReferralID
+    let hasPerk: Bool
+    let venuePage: VenuePage?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case location
+        case categories
+//        case referralID = "referralId"
+        case hasPerk, venuePage
+    }
+}
+
+struct Location: Codable {
+    let address, crossStreet: String?
+    let lat: Double
+    let lng: Double
+    let postalCode: String?
+    let cc: String?
+    let city: String?
+    let state: String
+    let country: String
+    let formattedAddress: [String]
+    let neighborhood: String?
+}
+
+struct Category: Codable {
+    let id: String
+    let name: String
+    let pluralName: String
+    let shortName: String
+    let icon: Icon
+    let primary: Bool
+}
+
+struct Icon: Codable {
+    let iconPrefix: String
+    let suffix: String
+
+    enum CodingKeys: String, CodingKey {
+        case iconPrefix = "prefix"
+        case suffix
+    }
+}
+
+struct VenuePage: Codable {
+    let id: String
+}
+
+
+
+
--- SuperTerrificHappyFourSquare/SuperTerrificHappyFourSquare/Network/FourSqaureAPI.swift
+++ SuperTerrificHappyFourSquare/SuperTerrificHappyFourSquare/Network/FourSqaureAPI.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct FourSquareAPIClient {
+    
+    
+    
+    static func getRestaurants(query: String, completion: @escaping (Result<[Venue], AppError>) -> ()){
+        
+        
+        let endpointURL = "https://api.foursquare.com/v2/venues/search?client_id=\(APIKey.clientID)&client_secret=\(APIKey.clientSecret)&v=20200221&near=Queens&query=\(query)"
+        
+    
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        
+        
+        let request = URLRequest(url: url)
+        
+       
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let search = try JSONDecoder().decode(VenueData.self, from: data)
+                    completion(.success(search.response.venues))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+        
+        
+    }
+}
